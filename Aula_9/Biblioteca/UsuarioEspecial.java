7bfce4486934cfbd3dd97848fbdcd97b1b76dd90
Joselio Jr <128568546+JoselioJr@users.noreply.github.com>
2024-06-20 12:16:27 -0300

    Delete Aula_9/Biblioteca directory

--- Aula_9/Biblioteca/UsuarioEspecial.java
+++
@@ -1,5 +0,0 @@
-public class UsuarioEspecial extends Pessoa{
-    public UsuarioEspecial(String nome, String telefone, String email, String cpf) {
-        super(nome, telefone, email, cpf);
-    }
-}
