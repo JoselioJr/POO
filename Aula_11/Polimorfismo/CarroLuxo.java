80ab8b12c1125e11dd1bd7f9de1b2e56332f60f1
Joselio Jr. <128568546+JoselioJr@users.noreply.github.com>
2024-08-16 01:36:46 -0300

    Add files via upload

---
+++ Aula_11/Polimorfismo/CarroLuxo.java
@@ -0,0 +1,44 @@
+public class CarroLuxo extends Veiculo{
+    private int qtdAir;
+    private String tamanhoPortaMalas;
+    private boolean possuiGps;
+
+    public CarroLuxo(String marca, String modelo, String placa, int ano, double valor, double multa,
+            int qtdAir, String tamanhoPortaMalas, boolean possuiGps) {
+        super(marca, modelo, placa, ano, valor, multa);
+        this.qtdAir = qtdAir;
+        this.tamanhoPortaMalas = tamanhoPortaMalas;
+        this.possuiGps = possuiGps;
+    }
+
+    public int getQtdAir() {
+        return qtdAir;
+    }
+
+    public void setQtdAir(int qtdAir) {
+        this.qtdAir = qtdAir;
+    }
+
+    public String getTamanhoPortaMalas() {
+        return tamanhoPortaMalas;
+    }
+
+    public void setTamanhoPortaMalas(String tamanhoPortaMalas) {
+        this.tamanhoPortaMalas = tamanhoPortaMalas;
+    }
+
+    public boolean isPossuiGps() {
+        return possuiGps;
+    }
+
+    public void setPossuiGps(boolean possuiGps) {
+        this.possuiGps = possuiGps;
+    }
+
+    @Override
+    public double CalculoMulta(){
+        if (getRenovacoes()>5) {
+            return super.CalculoMulta();
+        }else return 0;
+    }
+}
